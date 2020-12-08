4ef3a2a5b6a5c7991db56803ac1340ef1b6a82cc
kparra7491 <kparra7491@gmail.com>
2020-12-08 17:28:34 +0000

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+(function() {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  var cc =
+    document.querySelectorAll("#inputCard") >= 10000000000000000 ? true : false;
+  var cvc = document.querySelectorAll("#inputCVC") >= 1000 ? true : false;
+  var zip = document.querySelectorAll("#inputZip") >= 100000 ? true : false;
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener(
+      "submit",
+
+      function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
